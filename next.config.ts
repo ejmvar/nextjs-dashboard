3100422e0faed391659b08fb27e1fc8e8020aa05
EM <ejmvar@gmail.com>
2025-03-28 16:14:22 -0300

    build: turbopack issues

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    turbo: {
+      resolveAlias: {
+        // Ensure consistent React Server Component bindings
+        'react-server-dom-webpack/server.edge': 'react-server-dom-turbopack/server.edge',
+        'react-server-dom-webpack': 'react-server-dom-turbopack'
+      }
+    }
+  }
 };
 
 export default nextConfig;
