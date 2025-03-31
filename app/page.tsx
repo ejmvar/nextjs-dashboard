1c7f43c28375c0da85ca33bf5e7401ec7eb7a396
EM <ejmvar@gmail.com>
2025-03-31 17:55:10 -0300

    fix: homa page message updated

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1 className="text-4xl sm:text-5xl font-bold tracking-[-.01em] text-center sm:text-left">
-          Create Next App
+          My newly created **Next App** with **Turbopack** and **Tailwind CSS**!
         </h1>
 
 
